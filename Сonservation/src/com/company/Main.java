63e1fe150743590cfd94499c132fb789d35673a7
AnastasiaDa <danilova12333@gmail.com>
2022-04-18 23:15:18 +0300

    change Conservation Main

--- Сonservation/src/com/company/Main.java
+++ Сonservation/src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -16,31 +18,19 @@ public class Main {
         }
     }
 
-    public static void zipFiles(String path, String string) throws IOException {
-        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path));
-             FileInputStream fis = new FileInputStream(string)) {
-            ZipEntry entry = new ZipEntry(string);
-            zout.putNextEntry(entry);
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            zout.write(buffer);
-            zout.closeEntry();
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(path))) {
-            ZipEntry entry;
-            String name;
-            while ((entry = zin.getNextEntry()) != null) {
-                name = entry.getName();
-                FileOutputStream fout = new FileOutputStream(name);
-                for (int c = zin.read(); c != -1; c = zin.read()) {
-                    fout.write(c);
-                }
-                fout.flush();
-                zin.closeEntry();
-                fout.close();
+    public static void zipFiles(String path, List<String> list) throws IOException {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path))) {
+            for (String fileZip : list) {
+                File file = new File(fileZip);
+                String zName = file.getName();
+                FileInputStream fis = new FileInputStream(fileZip);
+                ZipEntry entry = new ZipEntry("packed_" + zName);
+                zout.putNextEntry(entry);
+                byte[] buffer = new byte[fis.available()];
+                fis.read(buffer);
+                zout.write(buffer);
+                zout.closeEntry();
+                fis.close();
             }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
@@ -54,20 +44,29 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        GameProgress prog1 = new GameProgress(15,4, 3, 5.5);
+        GameProgress prog1 = new GameProgress(15, 4, 3, 5.5);
         GameProgress prog2 = new GameProgress(10, 6, 6, 7.8);
         GameProgress prog3 = new GameProgress(20, 7, 10, 14.4);
 
-        saveGame("C:/Games/savegames/save1.dat", prog1);
-        saveGame("C:/Games/savegames/save2.dat", prog2);
-        saveGame("C:/Games/savegames/save3.dat", prog3);
+        File save1 = new File("C:/Games/savegames/save1.dat");
+        File save2 = new File("C:/Games/savegames/save2.dat");
+        File save3 = new File("C:/Games/savegames/save3.dat");
+        File saveZip = new File("C:/Games/savegames/zip.zip");
 
-        zipFiles("C:/Games/savegames/zip.zip","C:/Games/savegames/save1.dat");
-        zipFiles("C:/Games/savegames/zip.zip","C:/Games/savegames/save2.dat");
-        zipFiles("C:/Games/savegames/zip.zip","C:/Games/savegames/save3.dat");
+        saveGame(save1.getAbsolutePath(), prog1);
+        saveGame(save2.getAbsolutePath(), prog2);
+        saveGame(save3.getAbsolutePath(), prog3);
 
+        List<String> saveFiles = new ArrayList<>();
+        saveFiles.add(save1.getAbsolutePath());
+        saveFiles.add(save2.getAbsolutePath());
+        saveFiles.add(save3.getAbsolutePath());
 
+        zipFiles(saveZip.getAbsolutePath(), saveFiles);
 
-
+        deleteFile(save1);
+        deleteFile(save2);
+        deleteFile(save3);
     }
 }
+
